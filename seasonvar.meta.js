8bfdd8ed10275ed02898f06326065debd9d65b99
pavel <work.mail.ru@yandex.ru>
2015-12-20 16:27:33 +0300

    add meta and update url

---
+++ seasonvar.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Seasonvar.ru
+// @namespace   http://blog.monstuff.com/archives/cat_greasemonkey.html
+// description Create direct urls to video instead of web player
+// @include     http://seasonvar.ru/
+// @match       http://seasonvar.ru/*
+// @version     0.0.1
+// updateURL       https://raw.githubusercontent.com/likwrk/userscripts/master/seasonvar.meta.js
+// @updateURL      https://raw.githubusercontent.com/likwrk/userscripts/master/seasonvar.meta.js
+// @downloadURL    https://raw.githubusercontent.com/likwrk/userscripts/master/seasonvar.user.js
+// @run-at document-end
+// ==/UserScript==
--- seasonvar.user.js
+++ seasonvar.user.js
@@ -4,6 +4,10 @@
 // description Create direct urls to video instead of web player
 // @include     http://seasonvar.ru/
 // @match       http://seasonvar.ru/*
+// @version     0.0.1
+// updateURL       https://raw.githubusercontent.com/likwrk/userscripts/master/seasonvar.meta.js
+// @updateURL      https://raw.githubusercontent.com/likwrk/userscripts/master/seasonvar.meta.js
+// @downloadURL    https://raw.githubusercontent.com/likwrk/userscripts/master/seasonvar.user.js
 // @run-at document-end
 // ==/UserScript==
 
